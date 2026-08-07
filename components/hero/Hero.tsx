04a9d8fc7fe78af88ad9b2e7c48536cba699ba1e
Gál Áron Szili <sziligalaron@gmail.com>
2026-08-07 11:18:43 +0200

    hero

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -3,6 +3,8 @@ import AtomIcon from "@/public/icons/HeroAtomIcon";
 import { motion, Variants } from "framer-motion";
 import Section from "@/lib/section";
 import Referencies from "./referencies/referencies";
+import Herobg from "@/public/herobg.svg";
+import Image from "next/image";
 
 export default function Hero() {
   const h1Items = ["Webapplikáció", "Webshop", "CRM", "Weboldal"];
@@ -36,41 +38,43 @@ export default function Hero() {
   };
 
   return (
-    <Section className="flex-col md:flex-row flex pt-[120px] pb-[35px] md:min-h-[100vh] min-h-[450px] items-center justify-center gap-[25px] md:max-h-[1200px]">
-      <div className="hero-bal w-full md:w-[50%] p-[10px] h-full flex-col flex justify-between gap-[20px]">
-        <div className="flex flex-row gap-[10px] rounded-md bg-black/50 text-white w-fit p-[5px] items-center ">
-          <AtomIcon />
-          <p>Egyedi fejlesztés</p>
+    <section className="w-full flex-col flex rounded-b-[50px] relative overflow-hidden zoldhatteres bg-dark-color">
+      <div className="w-[90%] max-w-[2560px] mx-auto flex-col flex md:flex-row  pt-[120px] pb-[35px] md:min-h-[100vh] min-h-[450px] items-center justify-center gap-[25px] md:max-h-[1200px] ">
+        <div className="hero-bal w-full md:w-[50%] p-[10px] h-full flex-col flex justify-between gap-[20px]">
+          <div className="flex flex-row gap-[10px] rounded-md bg-dark-green text-white w-fit p-[5px] items-center ">
+            <AtomIcon />
+            <p>Egyedi fejlesztés</p>
+          </div>
+
+          {/* Animált H1 szülő konténer */}
+          <motion.h1
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            className="font-black text-[20px] md:text-[35px] xl:!text-[75px] flex !flex-col gap-[10px]"
+          >
+            {h1Items.map((item, idx) => (
+              <motion.span
+                key={idx}
+                variants={itemVariants}
+                className="h1-elem origin-top-left inline-block relative text-white"
+              >
+                {item}
+                <span className="arrow-left"></span>
+                <span className="arrow-right"></span>
+              </motion.span>
+            ))}
+          </motion.h1>
+
+          <p className="text-[15px] md:text-[20px] text-white">
+            Vállalkozásod igényeire optimalizált digitális eszközök fejlesztése.
+          </p>
         </div>
 
-        {/* Animált H1 szülő konténer */}
-        <motion.h1
-          variants={containerVariants}
-          initial="hidden"
-          animate="visible"
-          className="font-black text-[20px] md:text-[35px] xl:!text-[75px] flex !flex-col gap-[10px]"
-        >
-          {h1Items.map((item, idx) => (
-            <motion.span
-              key={idx}
-              variants={itemVariants}
-              className="h1-elem origin-top-left inline-block relative"
-            >
-              {item}
-              <span className="arrow-left"></span>
-              <span className="arrow-right"></span>
-            </motion.span>
-          ))}
-        </motion.h1>
-
-        <p className="text-[15px] md:text-[20px]">
-          Vállalkozásod igényeire optimalizált digitális eszközök fejlesztése.
-        </p>
+        <div className="hero-jobb w-full md:w-[50%] ">
+          <Referencies></Referencies>
+        </div>
       </div>
-
-      <div className="hero-jobb w-full md:w-[50%] ">
-        <Referencies></Referencies>
-      </div>
-    </Section>
+    </section>
   );
 }
