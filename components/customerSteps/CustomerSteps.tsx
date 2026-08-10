7ddc9900cb1c453415dc88592a45fad30b63b6a1
Gál Áron Szili <sziligalaron@gmail.com>
2026-08-10 21:01:50 +0200

    pipa lecserélve motion div-re , jobb animáció végett

--- components/customerSteps/CustomerSteps.tsx
+++ components/customerSteps/CustomerSteps.tsx
@@ -41,7 +41,6 @@ export default function CustomerStepsComp() {
     hidden: { opacity: 0, scale: 0.85 },
     visible: {
       opacity: 1,
-
       scale: 1,
       transition: {
         duration: 0.5,
@@ -50,15 +49,41 @@ export default function CustomerStepsComp() {
     },
   };
 
+  // 1. Kisebbik szár animációja
+  const shortLineVariants: Variants = {
+    hidden: { clipPath: "inset(0% 100% 0% 0%)" },
+    visible: {
+      clipPath: "inset(0% 0% 0% 0%)",
+      transition: {
+        duration: 0.15,
+        ease: "linear",
+        delay: 0.4,
+      },
+    },
+  };
+
+  // 2. Hosszabbik szár animációja
+  const longLineVariants: Variants = {
+    hidden: { clipPath: "inset(0% 100% 0% 0%)" },
+    visible: {
+      clipPath: "inset(0% 0% 0% 0%)",
+      transition: {
+        duration: 0.25,
+        ease: "easeOut",
+        delay: 0.55,
+      },
+    },
+  };
+
   return (
     <Section className="flex flex-col gap-[25px] my-[50px]">
-      {/* A szülő  */}
+      {/* A szülő */}
       <div
         ref={sentryWrapperRef}
         className="flex flex-col gap-[20px] justify-between relative py-10"
       >
         {/* Háttér vonal */}
-        <div className="absolute left-1/2 -translate-x-1/2 top-0 w-[4px] h-full  hidden md:block ">
+        <div className="absolute left-1/2 -translate-x-1/2 top-0 w-[4px] h-full hidden md:block">
           <motion.div
             className="w-full bg-green origin-top rounded-full"
             style={{ height: heightProgress }}
@@ -73,15 +98,28 @@ export default function CustomerStepsComp() {
             viewport={{ margin: "0px 0px -50% 0px", once: false }}
             initial="hidden"
             whileInView="visible"
-            className="flex flex-col p-[0px] rounded-md gap-[15px] w-full md:w-[50%] even:mr-auto even:pr-[50px] odd:ml-auto odd:pl-[50px]  relative z-10"
+            className="flex flex-col p-[0px] rounded-md gap-[15px] w-full md:w-[50%] even:mr-auto even:pr-[50px] odd:ml-auto odd:pl-[50px] relative z-10"
           >
+            {/* Fehér ikon kör háttere */}
             <motion.div
               variants={iconVariants}
               className={`absolute top-[50%] translate-y-[-50%] flex items-center justify-center p-[5px] bg-white z-20 w-fit rounded-full ${
                 index % 2 === 0 ? "right-[-20px]" : "left-[-20px]"
               }`}
             >
-              <CiBookmarkCheck size={30} className="text-green" />
+              <div className="relative w-[30px] h-[30px]">
+                {/* Rövid szár  */}
+                <motion.div
+                  variants={shortLineVariants}
+                  className="absolute h-[4px] w-[12px] bg-green rotate-[45deg] left-[4px] top-[16px] rounded-full"
+                />
+
+                {/* Hosszú szár  */}
+                <motion.div
+                  variants={longLineVariants}
+                  className="absolute h-[4px] w-[22px] bg-green rotate-[-50deg] left-[10px] top-[12px] rounded-full"
+                />
+              </div>
             </motion.div>
 
             <motion.h3 variants={itemVariants} className="text-xl font-bold">
