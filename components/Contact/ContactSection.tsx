9cdd706d73da949998f160450d636837300790c2
Gál Áron Szili <sziligalaron@gmail.com>
2026-08-13 15:18:25 +0200

    Kapcsolat szekció

--- components/Contact/ContactSection.tsx
+++ components/Contact/ContactSection.tsx
@@ -12,10 +12,10 @@ import ContactForm from "./ContacForm";
 
 export default function ContactSection() {
   const contactPrios = [
-    "Minőségi megjelenés",
-    " Céges imázs növelés",
+    "Minőségi termékek",
+    "Céges imázs növelés",
     "Hatékony munkaeszközök",
-    " Stabil partneri viszony",
+    "Stabil partneri viszony",
     "Kiszámítható működés",
   ];
 
@@ -48,7 +48,7 @@ export default function ContactSection() {
       opacity: 1,
       scale: 1,
       transition: {
-        duration: 0.5,
+        duration: 0.6,
         ease: [0.95, 0, 0.35, 1],
       },
     },
@@ -61,7 +61,7 @@ export default function ContactSection() {
       transition: {
         duration: 0.15,
         ease: "linear",
-        delay: 0.2,
+        delay: 0.6,
       },
     },
   };
@@ -73,31 +73,28 @@ export default function ContactSection() {
       transition: {
         duration: 0.25,
         ease: "easeOut",
-        delay: 0.4,
+        delay: 0.8,
       },
     },
   };
 
   return (
-    <section className="w-[90%] max-w-[2560px] mx-auto my-[50px] flex flex-col md:flex-row gap-[25px] bg-dark-color p-[10px] rounded-md  text-white font-bold ">
+    <section className="w-[90%] max-w-[2560px] mx-auto my-[50px] relative flex flex-col md:flex-row gap-[25px] bg-[#17202a] p-[10px] rounded-md  text-white font-bold ">
+      <div className="bg-white text-dark-color border-2 border-dark-color absolute top-0 left-1/2 -translate-x-1/2  -translate-y-1/2 p-[10px] rounded-md w-fit">
+        <h2 className="!text-[20px] font-black leading-tight ">Kapcsolat</h2>
+      </div>
+
       {/* Bal oldali infók --------- */}
       <div className="flex flex-col w-full md:w-1/2  gap-[20px] ">
-        <div className=" p-[10px] rounded-md w-fit">
-          <h2 className="!text-[20px]">
-            Növeld vállalkozásod hatásfokát egyedi fejlesztésű webes
-            megoldásaimmal
-          </h2>
-        </div>
-
         <div className="flex flex-col xl:flex-row gap-[15px] w-full">
           {/* Kép és elérhetőség ikonok ------- */}
-          <div className="w-full md:w-[350px] flex flex-col overflow-hidden rounded-md">
+          <div className="w-full md:w-[350px] flex flex-col">
             <Image
               alt="Egyedi weboldal fejlesztés, webshop fejlesztés, crm rendszer feljesztés"
               src={Kepem}
-              className="w-full  h-[350px] md:h-[250px] object-cover rounded-md"
-            ></Image>
-            <div className="flex-col md:flex-row flex gap-[10px] my-[20px] items-center justify-center">
+              className="w-full h-[350px] md:h-[350px] object-cover rounded-md"
+            />
+            <div className="flex-col md:flex-row flex gap-[10px] py-[20px] px-[10px] items-center justify-center border border-white border-t-0 rounded-b-md">
               <div className="flex flex-row gap-3 items-center">
                 <a
                   href="mailto:sziligalaron@gmail.com"
@@ -112,8 +109,8 @@ export default function ContactSection() {
                   <FiPhoneCall size={25} className="text-dark-color" />
                 </a>
               </div>
-              <div className="flex flex-col md:border-l-2 md:border-white pl-[10px] !text-neutral-200 ">
-                <p className="">Szili Gál Áron</p>
+              <div className="flex flex-col md:border-l-2 md:border-white pl-[10px] !text-neutral-200">
+                <p>Szili Gál Áron</p>
                 <p>FullStack fejlesztő</p>
               </div>
             </div>
@@ -122,36 +119,39 @@ export default function ContactSection() {
           {/* Kapcsolat előnyök ------ */}
           <div className="flex flex-col gap-[15px] p-[10px] rounded-md w-full md:w-fit">
             {contactPrios.map((prio, _) => (
-              <div key={prio} className="relative flex items-center w-full">
+              <div
+                key={prio}
+                className="relative flex items-stretch h-full w-full "
+              >
                 <motion.div
                   initial="hidden"
                   variants={wrapperVariants}
                   whileInView="visible"
                   viewport={{ margin: "0px 0px 0% 0px", once: false }}
-                  className="relative flex items-center"
+                  className="relative flex items-center w-full"
                 >
-                  {/* Ikon konténer */}
-                  <motion.div
-                    variants={iconVariants}
-                    className="flex items-center justify-center p-[5px] z-20 w-fit rounded-full"
-                  >
-                    <motion.div className="relative w-[30px] h-[30px]">
-                      <motion.div
-                        variants={shortLineVariants}
-                        className="absolute h-[4px] w-[12px] bg-green rotate-[45deg] left-[4px] top-[16px] rounded-full"
-                      />
-                      <motion.div
-                        variants={longLineVariants}
-                        className="absolute h-[4px] w-[22px] bg-green rotate-[-50deg] left-[10px] top-[12px] rounded-full"
-                      />
-                    </motion.div>
-                  </motion.div>
-
                   {/* Balról jobbra kitöltő zöld háttér */}
                   <motion.div
                     variants={containerVariant}
-                    className="bg-green rounded-full p-[10px] w-full md:w-fit"
+                    className="bg-green rounded-full p-[10px] w-full md:w-full flex flex-row items-center gap-[10px]"
                   >
+                    {/* Ikon konténer */}
+                    <motion.div
+                      variants={iconVariants}
+                      className="flex items-center justify-center p-[5px] z-20 w-fit rounded-full bg-white"
+                    >
+                      <motion.div className="relative w-[30px] h-[30px]">
+                        <motion.div
+                          variants={shortLineVariants}
+                          className="absolute h-[4px] w-[12px] bg-green rotate-[45deg] left-[4px] top-[16px] rounded-full"
+                        />
+                        <motion.div
+                          variants={longLineVariants}
+                          className="absolute h-[4px] w-[22px] bg-green rotate-[-50deg] left-[10px] top-[12px] rounded-full"
+                        />
+                      </motion.div>
+                    </motion.div>
+
                     <p className="text-white">{prio}</p>
                   </motion.div>
                 </motion.div>
