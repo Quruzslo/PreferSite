645a04ef2e74d8313a6c527f26d2f84035a0b7a9
Gál Áron Szili <sziligalaron@gmail.com>
2026-09-17 17:16:22 +0200

    Előnyök szekció átdesignolva

--- components/BenefitSection/BenefitSection.jsx
+++ components/BenefitSection/BenefitSection.jsx
@@ -3,46 +3,70 @@
 import { useRef } from "react";
 import Section from "../../lib/section";
 import { benefits } from "./benefits";
-import { motion, useScroll, useTransform } from "motion/react";
+import { motion, useTransform, useMotionValue, useSpring } from "motion/react";
 
-const BenefitCard = ({ benefit }) => {
-  const ref = useRef(null);
+// const BenefitCard = ({ benefit }) => {
+//   const ref = useRef(null);
 
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "80% end"],
-  });
+//   const { scrollYProgress } = useScroll({
+//     target: ref,
+//     offset: ["start end", "80% end"],
+//   });
 
-  const filter = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["blur(8px)", "blur(0px)"],
-  );
-  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+//   const filter = useTransform(
+//     scrollYProgress,
+//     [0, 1],
+//     ["blur(8px)", "blur(0px)"],
+//   );
+//   const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
+//   return (
+//     <motion.div
+//       ref={ref}
+//       style={{
+//         filter,
+//         opacity,
+//         willChange: "filter, opacity",
+//       }}
+//       className="flex flex-col gap-4 benefit-card relative p-[10px] bg-dark-color rounded-[19px] text-white"
+//     >
+//       <span className="w-fit benefit-badge rounded-[11px] bg-green px-3 py-1 text-[20px] font-black uppercase text-white ">
+//         {benefit.badge}
+//       </span>
+//       <h3 className="text-xl font-bold bg-white ml-[-10px] my-[15px] w-fit text-dark-color rounded-tr-[20px] rounded-br-[20px] pr-[5px] py-[15px] benefit-title relative">
+//         {benefit.title}
+//       </h3>
+//       <p className="text-neutral-600 dark:text-neutral-100">
+//         {benefit.description}
+//       </p>
+//     </motion.div>
+//   );
+// };
+
+export const BenefitCard = ({ benefit }) => {
   return (
-    <motion.div
-      ref={ref}
-      style={{
-        filter,
-        opacity,
-        willChange: "filter, opacity",
-      }}
-      className="flex flex-col gap-4 benefit-card relative p-[10px] bg-dark-color rounded-[19px] text-white"
-    >
-      <span className="w-fit benefit-badge rounded-[11px] bg-green px-3 py-1 text-[20px] font-black uppercase text-white ">
-        {benefit.badge}
-      </span>
-      <h3 className="text-xl font-bold bg-white ml-[-10px] my-[15px] w-fit text-dark-color rounded-tr-[20px] rounded-br-[20px] pr-[5px] py-[15px] benefit-title relative">
-        {benefit.title}
-      </h3>
-      <p className="text-neutral-600 dark:text-neutral-100">
-        {benefit.description}
-      </p>
-    </motion.div>
+    <div className="benefit-parent !w-full">
+      <div className="benefit-card !w-full">
+        {/* <div className="benefit-bg-blur" /> */}
+
+        {benefit.badge && (
+          <div className="benefit-date-box w-fit">
+            <span className="benefit-badge !text-white">{benefit.badge}</span>
+          </div>
+        )}
+
+        {/* Alsó tartalom blokk */}
+        <div className="benefit-content-box">
+          <h3 className="benefit-title">{benefit.title}</h3>
+          <p className="benefit-content">{benefit.description}</p>
+          <a href="/kapcsolat" className="benefit-see-more !text-white">
+            Részletek
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
-
 export default function BenefitSection() {
   return (
     <Section className="flex flex-col gap-[40px] py-[50px]">
